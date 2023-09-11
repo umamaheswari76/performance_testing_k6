182878f4cefa8075bc2feb37359a4925d2d34c36
umamaheswari76 <93276474+umamaheswari76@users.noreply.github.com>
2023-09-11 18:14:47 +0530

    added folder

---
+++ k6_performance_test/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//10lac req with 100 thread
+
+func main() {
+	r := gin.Default()
+	r.POST("/k6test", func(c *gin.Context) {
+		// Parse the JSON request body
+		var requestBody map[string]interface{}
+		if err := c.ShouldBindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Modify the JSON data
+		requestBody["message"] = "Modified: " + requestBody["message"].(string)
+
+		// Send the modified JSON as a response
+		c.JSON(http.StatusOK, requestBody)
+	})
+
+	r.Run(":8080")
+}
